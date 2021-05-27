70515a111686eb22b090518e78b42ea48f317c31
elifbaydi <elifc.eilgaz@gmail.com>
2021-05-27 22:36:30 +0300

    ders 7 ödev 3 postmapping ve requestbody eklendi

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/JobSeekerController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobSeekerController.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.business.abstracts.JobSeekerService;
-import kodlamaio.hrms.core.adapter.MernisServiceAdapter;
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.entities.concretes.JobSeeker;
 
@@ -31,8 +32,8 @@ public class JobSeekerController {
 		return this.jobSeekerService.getAll();
 	}
 	
-	@GetMapping("/addjobseeker")
-	public void addJobSeeker(JobSeeker jobSeeker)
+	@PostMapping("/addjobseeker")
+	public void addJobSeeker(@RequestBody JobSeeker jobSeeker)
 	{
 		this.jobSeekerService.addJobSeeker(jobSeeker);
 	}
--- hrms/src/main/java/kodlamaio/hrms/api/controllers/UserController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/UserController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,8 +31,8 @@ public class UserController {
 			return this.userService.getAll();
 		}
 		
-		@GetMapping("/adduser")
-		public void addUser(User user)
+		@PostMapping("/adduser")
+		public void addUser(@RequestBody User user)
 		{
 			this.userService.insert(user);
 		}
